a920ef50a3dc5cfd1660c9cf5e1eb6afea1497cf
MasterLin <31615967@qq.com>
2024-08-30 17:28:11 +0800

    add feishu bot

--- src/controller/bot/Feishu.ts
+++ src/controller/bot/Feishu.ts
@@ -1,6 +1,8 @@
 import * as lark from '@larksuiteoapi/node-sdk';
 import config from '../../config';
 import DB from '../../util/postgres';
+import helper from "../../util/helper";
+import {ChatRequest} from "../../entity/chat_request";
 
 export default async (router: any): Promise<void> => {
   try {
@@ -13,7 +15,7 @@ export default async (router: any): Promise<void> => {
     for (const connector of connectors) {
       console.log("Feishu bot [" + connector.name + "] is connected...");
 
-      const { appId, appSecret, encryptKey } = connector.config;
+      const {appId, appSecret, encryptKey} = connector.config;
       const client = new lark.Client({
         appId,
         appSecret
@@ -24,55 +26,71 @@ export default async (router: any): Promise<void> => {
       if (encryptKey) params.encryptKey = encryptKey;
 
       const eventDispatcher = new lark.EventDispatcher(params).register({
-        'im.message.receive_v1': async data => { await receive(client, data) },
+        'im.message.receive_v1': data => {
+          receive(client, data, connector.name)
+        }, //  单聊
+        'im.chat.access_event.bot_p2p_chat_entered_v1': async data => {
+          await welcome(client, data)
+        },
       });
 
-      router.post(`/bot/feishu/${connector.name}/webhook/event`, lark.adaptKoaRouter(eventDispatcher, { autoChallenge: true, }));
+      router.post(`/bot/feishu/${connector.name}/webhook/event`, lark.adaptKoaRouter(eventDispatcher, {autoChallenge: true,}));
 
     }
   } catch (ex) {
-
+    console.error(ex);
   }
 }
 
+const welcome = async (client: lark.Client, data: any) => {
+  return null;
+};
 
-const receive = async (client: lark.Client, data: any) => {
-  console.log(data);
-  const open_chat_id = data.message.chat_id;
-  const content = data.message.content;
-  const jContent = JSON.parse(content);
+const chatSync = async (chatContent: string, session_id: string, connectorName: string) => {
+  const db = DB.build(config.pgsql);
+  const connector = await db.loadByKV('eiai_bot_connector', 'name', connectorName)
+  let chatRequest: ChatRequest;
+  chatRequest = {
+    model: "claude-3-sonnet",
+    messages: [
+      {
+        role: "user",
+        content: chatContent
+      }
+    ]
+  };
 
-  const res = await client.im.message.create({
-    params: {
-      receive_id_type: 'chat_id',
+  const response = await fetch("http://localhost:8866/v1/chat/completions", {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + connector.config.apiKey,
+      'Session-Id': session_id
     },
-    data: {
-      receive_id: open_chat_id,
-      content: JSON.stringify({
-        "schema": "2.0",
-        "config": {
-          "streaming_mode": true,
-        },
-        "card_link": {
-          "url": "https://www.baidu.com"
-        },
-        "header": {},
-        "body": {
-          "elements": [
-            {
-              "tag": "div",
-              "element_id": "xxxxx_id",
-              text: {
-                content: "Hello 123",
-                lines: 1
-              }
-            }
-          ]
-        }
-      }),
-      msg_type: 'interactive'
-    },
+    body: JSON.stringify(chatRequest)
   });
-  console.log(res);
-  return res;
+
+  return await response.json();
+}
+
+
+const receive = (client: lark.Client, data: any, connectorName: string) => {
+  const open_id = data.sender.sender_id.open_id;
+  const jContent = JSON.parse(data.message.content);
+  chatSync(jContent.text, data.message.chat_id, connectorName).then(async llmRes => {
+    await client.im.message.create({
+      params: {
+        receive_id_type: 'open_id',
+      },
+      data: {
+        receive_id: open_id,
+        msg_type: 'text',
+        content: JSON.stringify({
+          "text": llmRes.choices[0].message.content
+        }),
+        uuid: helper.generateUUID(),
+      }
+    });
+  })
 };
